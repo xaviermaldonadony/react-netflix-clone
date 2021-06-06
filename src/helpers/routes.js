2c929f7163a8a959cf0a3ca7d13f675eb2a2a1cb
Xavi <xaviermaldonadony@hotmail.com>
2021-06-05 23:20:33 -0400

    routes are using authentication, created custom hook it's an auth listner

---
+++ src/helpers/routes.js
@@ -0,0 +1,48 @@
+import { Route, Redirect } from 'react-router-dom';
+
+export function IsUserRedirect({ user, loggedInPath, children, ...rest }) {
+	// if we have a user and they go to sign in or sign up redirect them
+	// example, exact path='/something',
+	// will be passed as props
+	return (
+		<Route
+			{...rest}
+			render={() => {
+				console.log('user', user);
+				console.log(loggedInPath);
+				console.log(rest.path);
+				if (!user) {
+					return children;
+				}
+
+				if (user) {
+					console.log('i am a user');
+					return <Redirect to={{ pathname: loggedInPath }} />;
+				}
+				return null;
+			}}
+		/>
+	);
+}
+
+export function ProtectedRoute({ user, children, ...rest }) {
+	// if user logged in return the page (children)
+	return (
+		<Route
+			{...rest}
+			render={({ location }) => {
+				if (user) {
+					return children;
+				}
+
+				if (!user) {
+					return (
+						<Redirect to={{ pathname: 'signin', state: { from: location } }} />
+					);
+				}
+
+				return null;
+			}}
+		/>
+	);
+}

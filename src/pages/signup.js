bdd325d7613770e4551c0388871aeea036bdf028
Xavi <xaviermaldonadony@hotmail.com>
2021-06-08 00:45:26 -0400

    fixed typo on signup page (text)

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -72,7 +72,7 @@ export default function Signup() {
 							Sign Up
 						</Form.Submit>
 						<Form.Text>
-							Already a user? <Form.Link to='/signin'>Sign n now.</Form.Link>
+							Already a user? <Form.Link to='/signin'>Sign in now.</Form.Link>
 						</Form.Text>
 						<Form.TextSmall>
 							This page is protected by Google reCAPTCHA to ensure you're not a
